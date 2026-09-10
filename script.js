64e7ad91b1d7335fa8773be993ada514278ee404
puregrowfarm001-tech <puregrowfarm001@gmail.com>
2026-09-10 20:45:43 +0530

    update

--- script.js
+++ script.js
@@ -1354,8 +1354,7 @@ function printActiveAdminReport() {
   });
 
   const printWindow = window.open('', '_blank');
-  const basePath = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1);
-  const logoUrl = basePath + "mushroom/pgf logo.png";
+  
   printWindow.document.write(`
     <!DOCTYPE html>
     <html>
